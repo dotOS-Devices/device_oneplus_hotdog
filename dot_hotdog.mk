75197a8d960423f388778ce837f7bc52fa4cf09a
Mohan C M <mohanmanjappa@gmail.com>
2021-08-08 19:52:57 +0000

    hotdog: update redfin's august build fingerprint

--- dot_hotdog.mk
+++ dot_hotdog.mk
@@ -29,7 +29,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 TARGET_BOOT_ANIMATION_RES := 1440
 
 # Build info
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7TPro \
     PRODUCT_NAME=OnePlus7TPro \
